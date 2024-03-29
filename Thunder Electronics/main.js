bc40e76bf33134c212ae72fe54a5bb0e18c82dd3
Sahil <Sahilsid2001@gmail.com>
2024-03-29 12:18:01 +0530

    Fix the Layout Bug

--- Thunder Electronics/main.js
+++ Thunder Electronics/main.js
@@ -5,11 +5,11 @@ import { showProductsContainer } from "./homeProductsCards";
 // **************HAMBURGER*******************
 
 let Hamburger_icon = document.querySelector(".Hamburger-icon");
-// let All_ul = document.querySelector("ul");
+let All_ul = document.querySelector("ul");
 
 Hamburger_icon.addEventListener("click",()=>{
-    // All_ul.classList.toggle("showData")
-    console.log("Sahil")
+    All_ul.classList.toggle("showData")
+    console.log(All_ul);
 })
 
 
